252df2479890ebe1ead83fc88cb25c99a05a4a75
shz.ann91@gmail.com <shz.ann91@gmail.com>
2022-12-23 22:00:59 +0300

    first commit

--- js/style.js
+++ js/style.js
@@ -1,6 +1,10 @@
+const search = document.getElementById("search");
+const searchBox = document.getElementById("search-box");
+const closeSearch = document.getElementById("close-search")
+
+
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navBarLinks = document.getElementsByClassName("navbar")[0];
-const search = document.getElementById("search");
 const searchInput = document.getElementById("search-input");
 const line = document.getElementById("line");
 const heart_icon = document.getElementById("heart_icon");
@@ -10,9 +14,15 @@ const call = document.getElementById("call");
 const search_active = document.getElementById("search-active");
 const header = document.getElementById("header");
 const search_icon_active=document.getElementById("search_icon-active");
-const close = document.getElementById("close")
 
 
+search.addEventListener('click', function() {
+  searchBox.classList.remove('hidden')
+});
+
+closeSearch.addEventListener('click', function() {
+  searchBox.classList.add('hidden')
+});
 
 toggleButton.addEventListener("click", () => {
   navBarLinks.classList.toggle("active");
@@ -21,33 +31,3 @@ toggleButton.addEventListener("click", () => {
 function toggleNav(x) {
   x.classList.toggle("change");
 }
-
-search.addEventListener("click", () => {
-  searchInput.classList.toggle("active1");
-
-  // line.style.display = "none";
-  // heart_icon.style.display = "none";
-  logo.style.display = "none";
-  // navbar.style.display = "none";
-  // navbar.style.display = "none";
-   call.style.display = "none";
-  search_active.style.display="block";
-  header.style.backgroundColor="white";
-  // search.style.display="none";
-  navbar.style.display = "none"
-  search_icon_active.style.display="block";
-  close.style.display ="block"
-
-});
-close.addEventListener("click",()=>{
-  navBarLinks.classList.remove("active");
-  // searchInput.classList.remove("active1")
-  header.style.backgroundColor="black";
-  navbar.style.display = "block";
-  logo.style.display = "block";
-  call.style.display = "block";
-  close.style.display = "none"
-  searchInput.style.display = "none"
-
-
-})
